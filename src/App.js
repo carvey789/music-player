29448559367db6178652421bd3f75042e5ee2b35
carvey789 <carvey789@gmail.com>
2021-11-21 01:20:44 +0700

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import Card from "./components/Card";
 import data from './utils/data';
 import SongList from './components/SongList';
@@ -6,6 +6,7 @@ import SongList from './components/SongList';
 import './styles/App.css';
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
   const [songList, setSongList] = useState(data);
   const [song, setSong] = useState(songList[0]);
   
